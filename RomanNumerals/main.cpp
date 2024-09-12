cfaf1559ff2381bd98599de65fce7442e0848ae8
Josh <joshua.boycehyland15@gmail.com>
2024-09-12 22:08:22 +0100

    core functionality now correct

--- RomanNumerals/main.cpp
+++ RomanNumerals/main.cpp
@@ -14,34 +14,46 @@ int main()
     std::cout << "Please enter a Roman numeral to be translated: ";
     std::cin>> input;
 
+    while( input.length() > 15 )
+    {
+        std::cout << "Please enter a Roman numeral under 15 characters: ";
+        std::cin>> input;
 
-        while( input.length() > 15 )
+    }
+
+    int translatedRomanNumeral = 0;
+
+    int year = 0;
+
+    char previousRomanNumeral;
+
+    for( int  i = input.length() - 1; i >= 0; i-- )
+    {
+        for(  int j = 0; j < 7;j++  )
         {
-            std::cout << "Please enter a Roman numeral under 15 characters: ";
-            std::cin>> input;
+            // compares against our roman numerals and adds corresponding value to total
+            if( romanNumeralStr[j] == input[i] ) {
 
-        }
-
-        int translatedRomanNumeral = 0;
-
-        int year = 0;
-        for( int  i = input.length() - 1; i >= 0; i-- )
-        {
-            for(  int j = 0; j < 7;j++  )
-            {
-                // compares against our roman numerals and adds corresponding value to total
-                if( romanNumeralStr[j] == input[i] ) {
-
-                    if( romanNumeralValues[j] < year ) {
-                        year -= romanNumeralValues[j];
-                    }
-                    else {
-                        year += romanNumeralValues[j];
-                    }
+                // decides whether we must minus the next number or add it to the year
+                if( romanNumeralValues[j] < year && romanNumeralStr[j] != previousRomanNumeral ) {
+                    year -= romanNumeralValues[j];
                 }
+                else {
+                    year += romanNumeralValues[j];
+                }
+                previousRomanNumeral = romanNumeralStr[j];
+                
             }
         }
-    std::cout<< " year: " << year;
+    }
+    //MMif( year < 4000 ) {
+        std::cout<< " year: " << year;
+    // }
+    // else {
+    //     std::cout <<"Year is outside of range: 1 - 3999"<<std::endl;
+    // }
+
+
 
     return 0;
 }
