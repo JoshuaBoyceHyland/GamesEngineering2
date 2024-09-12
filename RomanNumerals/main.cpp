d233aa6b8f6ecfb5f6aafbbbaed8db0e6051f0d8
Josh <joshua.boycehyland15@gmail.com>
2024-09-12 21:16:52 +0100

    Was not working previously, fixed now

--- RomanNumerals/main.cpp
+++ RomanNumerals/main.cpp
@@ -5,6 +5,7 @@
 int main()
 {
     std::string input;
+    bool keepTranslating = true;
 
     // Roman numerals and their corresponding values
     char romanNumeralStr[7] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
@@ -13,28 +14,34 @@ int main()
     std::cout << "Please enter a Roman numeral to be translated: ";
     std::cin>> input;
 
-    while( input.length() > 15 )
-    {
-        std::cout << "Please enter a Roman numeral under 15 characters: ";
-        std::cin>> input;
 
-    }
-
-    int translatedRomanNumeral = 0;
-
-    for( char individualInput : input )
-    {
-        for(  int i = 0; i < 7; i++  )
+        while( input.length() > 15 )
         {
-            // compares against our roman numerals and adds corresponding value to total
-            if( romanNumeralStr[i] == individualInput )
+            std::cout << "Please enter a Roman numeral under 15 characters: ";
+            std::cin>> input;
+
+        }
+
+        int translatedRomanNumeral = 0;
+
+        int year = 0;
+        for( int  i = input.length() - 1; i >= 0; i-- )
+        {
+            for(  int j = 0; j < 7;j++  )
             {
-                translatedRomanNumeral += romanNumeralValues[i];
+                // compares against our roman numerals and adds corresponding value to total
+                if( romanNumeralStr[j] == input[i] ) {
+
+                    if( romanNumeralValues[j] < year ) {
+                        year -= romanNumeralValues[j];
+                    }
+                    else {
+                        year += romanNumeralValues[j];
+                    }
+                }
             }
         }
-    }
-
-    std::cout<<"Your translated Roman Numeral is: " <<translatedRomanNumeral;
+    std::cout<< " year: " << year;
 
     return 0;
 }
