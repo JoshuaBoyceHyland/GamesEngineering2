d178c879d79b8c21010cb07c7989ccc34151d707
Josh <joshua.boycehyland15@gmail.com>
2024-09-12 20:19:40 +0100

    Rough code but working solution

--- RomanNumerals/main.cpp
+++ RomanNumerals/main.cpp
@@ -5,24 +5,36 @@
 int main()
 {
     std::string input;
-    char RomanNumeralStr[7] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
+
+    // Roman numerals and their corresponding values
+    char romanNumeralStr[7] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
     int romanNumeralValues[7] = { 1, 5, 10, 50, 100, 500, 1000 };
 
-    std::cout << "Please enter a Roman numeral under 15 characters: ";
+    std::cout << "Please enter a Roman numeral to be translated: ";
     std::cin>> input;
 
-    int final = 0;
+    while( input.length() > 15 )
+    {
+        std::cout << "Please enter a Roman numeral under 15 characters: ";
+        std::cin>> input;
+
+    }
+
+    int translatedRomanNumeral = 0;
 
     for( char individualInput : input )
     {
         for(  int i = 0; i < 7; i++  )
         {
-            if( RomanNumeralStr[i] == individualInput )
+            // compares against our roman numerals and adds corresponding value to total
+            if( romanNumeralStr[i] == individualInput )
             {
-                final += romanNumeralValues[i];
+                translatedRomanNumeral += romanNumeralValues[i];
             }
         }
     }
+
+    std::cout<<"Your translated Roman Numeral is: " <<translatedRomanNumeral;
 
     return 0;
 }
